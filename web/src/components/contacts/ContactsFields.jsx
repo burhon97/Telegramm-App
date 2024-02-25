5f4d3a976502f9539d941ccc22e1e8cd08cd167d
burhon97 <idriskhonovdev97@gmail.com>
2024-02-25 05:50:22 +0500

    fixed add-contact modal

--- web/src/components/contacts/ContactsFields.jsx
+++ web/src/components/contacts/ContactsFields.jsx
@@ -6,12 +6,11 @@ import { ModalContacts } from "./modal contacts/Modal-contacts";
 export default function ContactsFields({ onSelectContact, inputRef }) {
   const [showModal, setShowModal] = useState(false);
   const [contacts, setContacts] = useState([]);
-  const [refreshContact, setRefreshContact] = useState([])
-  
+  const [refreshContact, setRefreshContact] = useState([]);
 
-  useEffect(() => {  
+  useEffect(() => {
     fetch("http://localhost:4001/contacts-list")
-      .then((res) => res.json()) 
+      .then((res) => res.json())
       .then((loadContacts) => setContacts(loadContacts));
   }, [refreshContact]);
 
@@ -22,18 +21,17 @@ export default function ContactsFields({ onSelectContact, inputRef }) {
     onSelectContact(contact);
     // inputRef.current.focus();
   };
-  
 
   const onClickModalContacts = () => {
-    setShowModal(true);
+      setShowModal(true);
   };
 
   const onLogout = () => {
-    localStorage.removeItem('user')
-  }
+    localStorage.removeItem("user");
+  };
 
   return (
-    <form className="contacts-fields">
+    <form className="contacts-fields" onSubmit={(e)=> e.preventDefault()}>
       <button className="add-contact-modal" onClick={onClickModalContacts}>
         Add contact
       </button>
@@ -53,14 +51,18 @@ export default function ContactsFields({ onSelectContact, inputRef }) {
 
       {showModal && (
         <ModalContacts
-          showModal={showModal}
           setShowModal={setShowModal}
           contacts={contacts}
           setContacts={setContacts}
-          refreshContacts = {(refresh)=> setRefreshContact(refresh)}
+          refreshContacts={(refresh) => setRefreshContact(refresh)}
         />
       )}
-      <button onClick={onLogout} style={{position: 'absolute', bottom: '0', left: '40%'}}>Logout</button>
+      <button
+        onClick={onLogout}
+        style={{ position: "absolute", bottom: "0", left: "40%" }}
+      >
+        Logout
+      </button>
     </form>
   );
 }
--- web/src/components/contacts/modal contacts/Modal-contacts.jsx
+++ web/src/components/contacts/modal contacts/Modal-contacts.jsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom";
 import "./ModalContacts.css";
 import { useState } from "react";
 
-
 export const ModalContacts = ({ setShowModal, refreshContacts }) => {
   // const [addContactStatus, setAddContactStatus] = useState(false);
   // const [oldContacts, setOldContacts] = useState([]);
@@ -13,62 +12,57 @@ export const ModalContacts = ({ setShowModal, refreshContacts }) => {
   const onClickCancel = () => {
     setShowModal(false);
   };
-  
 
   const onClickAddContact = () => {
     fetch(
       `http://localhost:4001/contacts-save?name=${name}&&surname=${surname}&&phoneNumber=${phonenumber}`
     );
-    // setAddContactStatus(true);
     fetch("http://localhost:4001/contacts-list")
-      .then((res) => res.json()) 
+      .then((res) => res.json())
       .then((loadContacts) => refreshContacts(loadContacts));
     setShowModal(false);
   };
 
-  return ReactDOM.createPortal(
-    <>
-      <div className="modal-contacts">
-        <div className="modal-container">
-          <button className="modal-cancel" onClick={onClickCancel}>
-            x
+  return (
+    <div className="modal-contacts">
+      <div className="modal-container">
+        <button className="modal-cancel" onClick={onClickCancel}>
+          x
+        </button>
+        <div className="modal-list">
+          <h1 style={{ textTransform: "uppercase" }}>Add contact</h1>
+          <input
+            type="text"
+            value={name}
+            onChange={(el) => setName(el.target.value)}
+            className="modal-input"
+            placeholder="name..."
+          />
+          <br />
+          <input
+            type="text"
+            value={surname}
+            onChange={(el) => setSurname(el.target.value)}
+            className="modal-input"
+            placeholder="surname..."
+          />
+          <br />
+          <input
+            type="number"
+            value={phonenumber}
+            onChange={(el) => setPhoneNumber(el.target.value)}
+            className="modal-input"
+            placeholder="phone number..."
+          />
+          <br />
+          <button className="modal-btn" onClick={onClickCancel}>
+            Cancel
           </button>
-          <div className="modal-list">
-            <h1 style={{ textTransform: "uppercase" }}>Add contact</h1>
-            <input
-              type="text"
-              value={name}
-              onChange={(el) => setName(el.target.value)}
-              className="modal-input"
-              placeholder="name..."
-            />
-            <br />
-            <input
-              type="text"
-              value={surname}
-              onChange={(el) => setSurname(el.target.value)}
-              className="modal-input"
-              placeholder="surname..."
-            />
-            <br />
-            <input
-              type="number"
-              value={phonenumber}
-              onChange={(el) => setPhoneNumber(el.target.value)}
-              className="modal-input"
-              placeholder="phone number..."
-            />
-            <br />
-            <button className="modal-btn" onClick={onClickCancel}>
-              Cancel
-            </button>
-            <button className="modal-btn" onClick={onClickAddContact}>
-              Add contact
-            </button>
-          </div>
+          <button className="modal-btn" onClick={onClickAddContact}>
+            Add contact
+          </button>
         </div>
       </div>
-    </>,
-    document.getElementById("modal-contacts")
+    </div>
   );
 };
